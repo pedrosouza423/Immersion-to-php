be7bfe0f5169cda74bcbba1bc4e7a613c855096b
pedrosouza423 <pedro.souza4237@gmail.com>
2021-04-11 00:18:36 -0300

    Foi criado a pasta do 2 curso

---
+++ 2-Avançando-PHP/listas.php
@@ -0,0 +1,12 @@
+<?php
+
+//Array e loop lista
+
+$idadeList = [19,20,17];
+
+for($i= 0; $i < count($idadeList); $i++){
+echo $idadeList[$i] . PHP_EOL;
+
+}
+
+//count(array) conta quantos elementos tem em um array
